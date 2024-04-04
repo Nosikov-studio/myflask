da134a479bb911f608cd44006e7999fcae280b17
Nosikov <89134038088@mail.ru>
2024-04-04 19:22:21 +0700

    4++

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from flask import Flask, jsonify
 import os
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return 'Helloooooooooooo+'
+
+
+@app.route('/about')
+def index():
+    return 'We accept the fight'
+
 
 if __name__ =="__main__":
     app.run(debug=True, port=os.getenv("PORT", default=5000))
