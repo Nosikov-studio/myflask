a2d95d6ff023f8188f4587f65dee39ff7e58ec19
Nosikov <89134038088@mail.ru>
2024-04-15 20:43:31 +0700

    +70

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ class Article(db.Model):
     title=db.Column(db.String(100), nullable=False)
     intro=db.Column(db.String(300), nullable=False)
     text=db.Column(db.Text, nullable=False)
-    #date=db.Column(db.DateTime, default=datetime.utcnow)
+    date=db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Article %r>' % self.id
